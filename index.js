c40ec2b491e3dc790870ad901f65811286b5ef66
Justin Molina <46831477+AlfredoMolina@users.noreply.github.com>
2021-04-07 00:04:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+
+// 
+const addRecipe = (recipe, id) => {
+
+    let time = recipe.created_at.toDate();
+
+    let html = `
+
+<li data-id="${id}">
+<div>${recipe.title}</div>
+<div>${time}</div>
+<button class="btn btn-danger btn-sm my-2"> Delete </button>
+</li>
+`;
+    list.innerHTML += html;
+}
+
+//delete/add functions
+const deleteRecipe = (id) => {
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+
+        if (recipe.getAttribute('data-id') === id) {
+            recipe.remove();
+        }
+    })
+}
+const unsub = db.collection('recipes').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+        if (change.type === 'added') {
+
+            addRecipe(doc.data(), doc.id);
+        } else if (change.type === 'removed') {
+            deleteRecipe(doc.id);
+        }
+    })
+});
+
+
+// add props
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+
+    };
+
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+
+    }).catch(err => {
+
+        console.log(err);
+    });
+});
+
+//delete props/event delegation
+list.addEventListener('click', e => {
+
+    if (e.target.tagName === 'BUTTON') {
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete();
+        console.log('recipe deleted');
+    }
+});
+
+//unsub from Database
+button.addEventListener('click', () => {
+
+    unsub();
+    console.log('unsubbed from DB changes');
+
+});
